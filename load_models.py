845280b835ff153c3a8ecda6fc854fbef76daf51
makoohara <97365398+makoohara@users.noreply.github.com>
2024-03-29 20:53:31 +0000

    heroku optimization

--- load_models.py
+++
@@ -1,8 +0,0 @@
-from allennlp.predictors.predictor import Predictor
-import allennlp_models.tagging
-
-
-#load the model
-SRL_MODEL_PATH = "https://storage.googleapis.com/allennlp-public-models/structured-prediction-srl-bert.2020.12.15.tar.gz"
-
-predictor = Predictor.from_path(SRL_MODEL_PATH)
