a789e2f3b5bdcb59d97c2f4a11726d1434db19c4
makoohara <97365398+makoohara@users.noreply.github.com>
2024-03-30 01:47:29 +0000

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -23,8 +23,8 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     @login_manager.user_loader
     def load_user(user_id):
