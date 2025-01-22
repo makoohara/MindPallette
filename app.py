d610c25e0f38ecde763b873f595320052260235e
makoohara <97365398+makoohara@users.noreply.github.com>
2025-01-22 15:31:35 +0900

    updates app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = create_app()
 CORS(app)  # Enable CORS for all routes
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Enable debug mode for development 
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Enable debug mode for development 
